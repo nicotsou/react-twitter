f92fb3189ae01fd2bccc6d2b87f638cc22e5c481
Nicos Tsourektsidis <creatorgr@hotmail.com>
2021-05-01 18:47:29 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return <div className="App">no more hello worlds please</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
